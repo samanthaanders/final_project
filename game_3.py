cafc22fac65429f4713bb061d0ad8b8754b1cd76
samanthaanders <112581381+samanthaanders@users.noreply.github.com>
2022-12-20 13:41:19 -0700

    draws an epic rectangle

--- game_3.py
+++ game_3.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -10,13 +11,106 @@ from pygame.locals import (
 )
 pygame.init()
 
+class item:
+    def __init__(self, name, cost, uses, desc, amount):
+        self.name = name
+        self.cost = cost
+        self.uses = uses
+        self.desc = desc
+        self.amount = amount
+
+class special_item(item): # inheritance 
+    def __init__(self, name, cost, uses, desc, amount):
+        super().__init__(name, cost, uses, desc, amount)
+    def rand_amount(self):
+        self.amount = random.randint(1,10)
+        return self.amount
+
+class player(pygame.sprite.Sprite):
+    def __init__(self, name, strength, health, num, money, power):
+        super(player, self).__init__()
+        self.name = name
+        self.strength = strength
+        self.health = health
+        self.num = num # used for randomizing damage dealt in attacks
+        self.money = money
+        self.power = power
+        self.obj_item = special_item(None, 0, 0, None, 1) # composition
+        self.sprite = pygame.Surface((50, 50)) # create character sprite
+        self.sprite.fill((0,0,0))
+        self.hitbox = self.sprite.get_rect() # hitbox = rect
+    def take_damage(self, num, damage, name):
+        print(name, "attacked!")
+        if (self.num == num):
+            self.health -= damage
+            print("the attack was very strong!")
+        elif ((self.num - num == 1) or (self.num - num == -1)): # if the numbers are within 1 of eachother, 1/2 damage is dealt
+            self.health -= damage / 2
+            print("the attack was strong")
+        elif ((self.num - num == 2) or (self.num - num == -2)): # if the numbers are within 2 of eachother, 1/4 damage is dealt
+            self.health -= damage / 4
+            print("the attack was weak")
+        else:
+            print("the attack failed!")
+    def check_health(self):
+        if (self.health <= 0):
+            print(self.name,"lost!")
+        else:
+            print(self.name + "'s health is: ", self.health)
+    def use_power(self, uses, amount):
+        if (self.power == "strength potion"):
+            if uses > 0:
+                self.strength += amount
+                print(self.name +"'s strength:", self.strength)
+                print(self.power +" uses left: ",(uses - 1))
+            else:
+                print(self.power + " has been used up")
+        elif (self.power == "health potion"):
+            if uses > 0:
+                self.health += amount
+                print(self.name +"'s health:", self.health)
+                print(self.power +" uses left: ",(uses - 1))
+            else:
+                print(self.power + " has been used up")
+        elif (self.power == "cookie"):
+            if uses > 0:
+                self.health += amount
+                print(self.name +"'s health:", self.health)
+                print(self.power +" uses left: ",(uses - 1))
+            else:
+                print(self.power + " has been used up")
+        elif (self.power == "sword"):
+            if uses > 0:
+                self.strength += amount
+                print(self.name +"'s strength:", self.strength)
+                print(self.power +" uses left: ",(uses - 1))
+            else:
+                print(self.power + " has already been equipped")
+        elif (self.power == "health spell"):
+            if uses > 0:
+                self.health += amount
+                print(self.name +"'s health:", self.health)
+                print(self.power +" uses left: ",(uses - 1))
+            else:
+                print(self.power + " has been used up")
+        elif (self.power == "luck potion"):
+            if uses > 0:
+                amount = self.obj_item.rand_amount() # composition to use method of "special_item" class
+                self.health += amount
+                print(self.name +"'s health:", self.health)
+                print(self.power +" uses left: ",(uses - 1))
+            else:
+                print(self.power + " has been used up")
+    def attack(self):
+        return random.randint(1,4)
+
 screen = pygame.display.set_mode((1000,600), pygame.RESIZABLE)
 pygame.display.set_caption("game")
 white = (255,255,255)
 screen.fill(white)
-p1_sprite = surf = pygame.Surface((50, 50))
-p1_sprite.fill((0,0,0))
 pygame.display.flip()
+
+p1 = player("name", 5, 10, random.randint(1,4),0, None)
 
 running = True
 while running == True:
@@ -27,8 +121,7 @@ while running == True:
             if event.key == K_ESCAPE:
                 running = False
     screen.fill(white)
-    p1_sprite_center = (((screen.get_width() - p1_sprite.get_width()) / 2), (screen.get_height() - p1_sprite.get_height()) / 2)
-    screen.blit(p1_sprite, p1_sprite_center)
+    screen.blit(p1.sprite, p1.hitbox) # sprite goes to top left corner
     pygame.display.flip()
 
 pygame.quit()
